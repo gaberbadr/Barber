41d309102ac6fd706c094f7ed464a3c8d6840c0b
gaber badr <gaberemadbader@gmail.com>
2026-08-09 21:30:23 +0300

    v1.5.0

--- Infrastructure/Persistence/Seed/DefaultAdminSeeder.cs
+++ Infrastructure/Persistence/Seed/DefaultAdminSeeder.cs
@@ -14,32 +14,70 @@ namespace Infrastructure.Persistence.Seed
             // 1. Seed roles
             await DefaultRolesSeeder.SeedRolesAsync(roleManager);
 
-            // 2. Create Admin user if no users exist
-            if (!userManager.Users.Any())
+            // 2. Seed default admins
+            var admins = new[]
             {
-                var user = new ApplicationUser
+                new
                 {
                     UserName = "gaberemadbader@gmail.com",
+                    Email = "gaberemadbader@gmail.com",
                     PhoneNumber = "01019806684",
                     FullName = "Gaber Emad Badr",
-                    Email = "gaberemadbader@gmail.com",
-                    EmailConfirmed = true,
-                };
-
-                var createResult = await userManager.CreateAsync(user, "Admin@123");
-
-                if (createResult.Succeeded)
+                    Password = "Admin@123"
+                },
+                new
                 {
-                    await userManager.AddToRoleAsync(user, "Admin");
+                    UserName = "ahmedmegahed580@gmail.com",
+                    Email = "ahmedmegahed580@gmail.com",
+                    PhoneNumber = "01000000000",
+                    FullName = "Ahmed Megahed",
+                    Password = "Admin@123"
+                },
+                 new
+                {
+                    UserName = "barbershopgm00@gmail.com",
+                    Email = "barbershopgm00@gmail.com",
+                    PhoneNumber = "01000000000",
+                    FullName = "owner",
+                    Password = "Admin@123"
                 }
-                else
+
+            };
+
+            foreach (var admin in admins)
+            {
+                // Check by email so the seeder doesn't create duplicates
+                var existingUser = await userManager.FindByEmailAsync(admin.Email);
+
+                if (existingUser == null)
                 {
-                    foreach (var error in createResult.Errors)
+                    var user = new ApplicationUser
                     {
-                        System.Console.WriteLine($"Error: {error.Code} - {error.Description}");
+                        UserName = admin.UserName,
+                        PhoneNumber = admin.PhoneNumber,
+                        FullName = admin.FullName,
+                        Email = admin.Email,
+                        EmailConfirmed = true
+                    };
+
+                    var createResult = await userManager.CreateAsync(user, admin.Password);
+
+                    if (createResult.Succeeded)
+                    {
+                        await userManager.AddToRoleAsync(user, "Admin");
+                    }
+                    else
+                    {
+                        foreach (var error in createResult.Errors)
+                        {
+                            System.Console.WriteLine(
+                                $"Error creating {admin.Email}: {error.Code} - {error.Description}"
+                            );
+                        }
                     }
                 }
             }
         }
     }
 }
+
